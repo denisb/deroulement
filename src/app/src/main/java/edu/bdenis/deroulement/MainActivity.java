e89027f2fb6c3657c9ca4d25faa8e1318ba04100
denisb <denis.bouhineau@imag.fr>
2018-08-16 17:41:35 +0200

    V0.1 première version testable

--- src/app/src/main/java/edu/bdenis/deroulement/MainActivity.java
+++ src/app/src/main/java/edu/bdenis/deroulement/MainActivity.java
@@ -1,5 +1,18 @@
 package edu.bdenis.deroulement;
 
+// TODO: 16/08/2018 : ajouter une numérotation automatique des titres et contenus
+// TODO: 16/08/2018 : gérer les plans se déroulant sur plusieurs séances
+// TODO: 16/08/2018 : ajouter une url ou un page avec un contenu fourni in extenso pour chaque item de contenu
+// TODO: 16/08/2018 : ajouter un splash screen de lancement
+// TODO: 16/08/2018 : ajouter une aide ou description
+// TODO: 16/08/2018 : ajouter un "qui sommes-nous ?"
+// TODO: 16/08/2018 : ajouter des parametres pour le nombre de plans disponible
+// TODO: 16/08/2018 : ajouter des parametres pour la taille des textes
+// TODO: 16/08/2018 : enlever le bandeau du haut "Déroulement (j'adore ...)" (il ne sert à rien)
+// TODO: 16/08/2018 : enregistrer les temps passés sur chaque contenu (si on sait quoi faire avec ensuite), ou seulement tracer l'activité ... 
+// TODO: 16/08/2018 : améliorer icone
+// TODO: 16/08/2018 : améliorer déplacement (2 dgts) vs observation (1 dgt)
+
 import android.app.AlertDialog;
 import android.content.ContentResolver;
 import android.content.Context;
@@ -13,11 +26,10 @@ import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.InputType;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -29,6 +41,8 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
@@ -43,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
     private String contenuText;
     private static final List<String> listStrings = new ArrayList<String>();
     private int indexStrings=0;
+    private int indexPlan =0;
     private int indexContenu=1;
     private int indexSousTitre=1;
     private int nbContenus=2;
@@ -51,21 +66,48 @@ public class MainActivity extends AppCompatActivity {
     private int charContenu=45;
     private int decalageTps = 0;
     private int decalageTpsInit = 0;
+    private int changeTps = 0;
     long startTime = 0;
     private float x1,x2;
     static final int MIN_DISTANCE = 10;
     Handler timerHandler = new Handler();
+    Runnable lightRunnable = new Runnable() {
+        @Override
+        public void run() {
+            WindowManager.LayoutParams lp = getWindow().getAttributes();
+            lp.screenBrightness = 0.0f;
+            getWindow().setAttributes(lp);}};
     Runnable timerRunnable = new Runnable() {
         @Override
         public void run() {
             long millis = System.currentTimeMillis() - startTime;
             int seconds = (int) (millis / 1000);
             int minutes = seconds / 60; //minutes passées depuis l'heure de début
-            int h = 0; int m = 0;
+            int h = 0; int m = 0; int hd, hf, md, mf;
             Calendar calendar = Calendar.getInstance();
             h = calendar.getTime().getHours();
             m = calendar.getTime().getMinutes();
-            int rm = (90*60-seconds) / 60; //minutes restantes jusqu'à l'heure de fin
+            int dureeS = 90*60;
+            int dureeMS = 1000 * dureeS;
+            int rm = (dureeS-seconds) / 60; //minutes restantes jusqu'à l'heure de fin
+            Pattern regExHeure = Pattern.compile("([^0-9]*)([0-9]+)([^0-9]+)([0-9]+)([^0-9]*)");
+            Matcher matchHeure = regExHeure.matcher(heureDebut);
+            if (matchHeure.matches()) {
+                hd = Integer.parseInt(matchHeure.group(2));
+                md = Integer.parseInt(matchHeure.group(4));}
+            else {
+                hd = 0; md = 0; heureDebut= "08h00";}
+            matchHeure = regExHeure.matcher(heureFin);
+            if (matchHeure.matches() && (matchHeure.groupCount()>2)) {
+                hf = Integer.parseInt(matchHeure.group(2));
+                mf = Integer.parseInt(matchHeure.group(4));}
+            else {
+                hf = 0; mf = 0; heureFin= "09h30";}
+            if (((h>hd)||((h==hd)&&(m>=md)))&&((h<hf)||((h==hf)&&(m<=mf)))) {
+                minutes = 60 * (h-hd) + m - md;
+                dureeS = 60*(hf-hd)+mf-md ; //duree reelle en minute !
+                dureeMS = 1000*60*dureeS+changeTps;  //duree modifiée/changée en ?!?
+                rm = dureeS - minutes;}
             Button boutonHeureDebut=(Button)findViewById(R.id.buttonHeureDebut);
             boutonHeureDebut.setX(20);
             boutonHeureDebut.setY(timerTextView.getY());
@@ -77,28 +119,37 @@ public class MainActivity extends AppCompatActivity {
             boutonHeureFin.setY(timerTextView.getY());
             boutonHeureFin.setText(heureFin+("-("+rm+")"));
             titreTextView.setText(titreText);
-            titreTextView.setX(220-Math.round((200*(millis+decalageTps))/(90*60*1000)));
+            titreTextView.setX(220-Math.round((200*(millis+decalageTps+changeTps))/dureeMS));
             indexContenu = -1;
             indexSousTitre = -1;
             for(int k = 0; k < listStrings.size(); k++) {
                 String se =  listStrings.get(k);
                 if (se.charAt(0)==charContenu) {
                     indexContenu++;
-                    if (indexContenu==Math.round(((millis+decalageTps)*nbContenus)/(90*60*1000))) {
+                    if (indexContenu==Math.round(((millis+decalageTps+changeTps)*nbContenus)/dureeMS)) {
                         contenuText = se;
                         soustitreTextView.setText(soustitreText);
-                        soustitreTextView.setX(220-Math.round((200*nbSousTitres)*(millis+decalageTps-((indexSousTitre*90*60*1000)/nbSousTitres))/(90*60*1000) ));
+                        soustitreTextView.setX(220-Math.round((200*nbSousTitres)*(millis+decalageTps+changeTps-((indexSousTitre*dureeMS)/nbSousTitres))/dureeMS ));
                         contenuTextView.setText(contenuText+" <<"+listStrings.get(k+1));
-                        contenuTextView.setX(220-Math.round((200*nbContenus)*(millis+decalageTps-((indexContenu*90*60*1000)/nbContenus))/(90*60*1000) ));
+                        contenuTextView.setX(220-Math.round((200*nbContenus)*(millis+decalageTps+changeTps-((indexContenu*dureeMS)/nbContenus))/dureeMS ));
                         break;}}
                 if (se.charAt(0)==charSousTitre) {
                     indexSousTitre++;
                     soustitreText = se;}}
-            decalageTps = (95 * decalageTps) / 100;
+            if (decalageTps>0) {
+                decalageTps = decalageTps - Math.min((3 * decalageTps / 100),15000);}
+            else {
+                decalageTps = decalageTps - Math.max((3 * decalageTps / 100),-15000);}
             timerHandler.postDelayed(this, 50);}};
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        float deltaX;
+        timerHandler.removeCallbacks(lightRunnable);
+        timerHandler.postDelayed(lightRunnable, 30000);
+        WindowManager.LayoutParams lp = getWindow().getAttributes();
+        lp.screenBrightness = 1.0f;
+        getWindow().setAttributes(lp);
         switch(event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 x1 = event.getX();
@@ -106,9 +157,15 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case MotionEvent.ACTION_MOVE:
                 x2 = event.getX();
-                float deltaX = x2 - x1;
+                deltaX = x2 - x1;
                 if (Math.abs(deltaX) > MIN_DISTANCE) {
-                    decalageTps=decalageTpsInit-(5000*Math.round(deltaX/MIN_DISTANCE));}
+                    if (event.getPointerCount()==1) {
+                        decalageTps=decalageTpsInit-(5000*Math.round(deltaX/MIN_DISTANCE));}
+                    else {
+                        x1 = x2;
+                        changeTps = changeTps + decalageTps;
+                        decalageTpsInit = 0;
+                        decalageTps=0;}}
                 break;}
         return super.onTouchEvent(event);}
 
@@ -116,23 +173,52 @@ public class MainActivity extends AppCompatActivity {
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            SharedPreferences sharedPref;
+            SharedPreferences.Editor editor;
+            Uri tmpSelectedfile;
+            String strUriPlanning;
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_previous:
+                    mTextMessage.setText(getText(R.string.title_previous)+" (>"+((indexPlan +4)%5)+")");
+                    indexPlan = (indexPlan +4) % 5;
+                    sharedPref = getPreferences(Context.MODE_PRIVATE);
+                    editor = sharedPref.edit();
+                    editor.putInt("saved_index_planning", indexPlan);
+                    editor.commit();
+                    heureDebut = sharedPref.getString("saved_heure_debut"+ indexPlan, "8h00");
+                    heureFin = sharedPref.getString("saved_heure_fin"+ indexPlan, "9h30");
+                    strUriPlanning = sharedPref.getString("saved_uri_selected"+ indexPlan, "/");
+                    tmpSelectedfile = Uri.parse(strUriPlanning);
+                    chargePlanning(tmpSelectedfile);
                     return true;
                 case R.id.navigation_main:
-                    mTextMessage.setText(R.string.title_main);
-                    return true;
-                case R.id.navigation_preferences:
-                    mTextMessage.setText(R.string.title_preferences);
+                    mTextMessage.setText(getText(R.string.title_main)+" ("+ indexPlan +")");
                     Intent intent = new Intent().setType("text/plain").setAction(Intent.ACTION_GET_CONTENT);
                     startActivityForResult(Intent.createChooser(intent, "Select a file"), 12345);
+                    return true;
+                case R.id.navigation_next:
+                    mTextMessage.setText(getText(R.string.title_next)+" (>"+((indexPlan +6)%5)+")");
+                    indexPlan = (indexPlan +6) % 5;
+                    sharedPref = getPreferences(Context.MODE_PRIVATE);
+                    editor = sharedPref.edit();
+                    editor.putInt("saved_index_planning", indexPlan);
+                    editor.commit();
+                    heureDebut = sharedPref.getString("saved_heure_debut"+ indexPlan, "8h00");
+                    heureFin = sharedPref.getString("saved_heure_fin"+ indexPlan, "9h30");
+                    strUriPlanning = sharedPref.getString("saved_uri_selected"+ indexPlan, "/");
+                    tmpSelectedfile = Uri.parse(strUriPlanning);
+                    chargePlanning(tmpSelectedfile);
                     return true;}
             return false;}};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        WindowManager.LayoutParams lp = getWindow().getAttributes();
+        lp.screenBrightness = 1.0f;
+        getWindow().setAttributes(lp);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        timerHandler.postDelayed(lightRunnable, 30000);
         setContentView(R.layout.activity_main);
         mTextMessage = (TextView) findViewById(R.id.message);
         timerTextView = (TextView) findViewById(R.id.heure);
@@ -142,12 +228,14 @@ public class MainActivity extends AppCompatActivity {
         soustitreText = getString(R.string.text_soustitre);
         contenuTextView = (TextView) findViewById(R.id.contenu);
         contenuText = getString(R.string.text_contenu);
-        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE); //getActivity().
-        heureDebut = sharedPref.getString("saved_heure_debut", "8h00");
-        heureFin = sharedPref.getString("saved_heure_fin", "9h30");
-        String strUriPlanning = sharedPref.getString("saved_uri_selected", "/");
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        indexPlan = sharedPref.getInt("saved_index_planning", 0);
+        heureDebut = sharedPref.getString("saved_heure_debut"+ indexPlan, "8h00");
+        heureFin = sharedPref.getString("saved_heure_fin"+ indexPlan, "9h30");
+        String strUriPlanning = sharedPref.getString("saved_uri_selected"+ indexPlan, "/");
         Uri tmpSelectedfile = Uri.parse(strUriPlanning);
         chargePlanning(tmpSelectedfile);
+        mTextMessage.setText(getText(R.string.title_main)+" ("+ indexPlan +")");
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
@@ -165,14 +253,14 @@ public class MainActivity extends AppCompatActivity {
                                     heureDebut = inputDialog.getText().toString();
                                     SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
                                     SharedPreferences.Editor editor = sharedPref.edit();
-                                    editor.putString("saved_heure_debut", heureDebut);
+                                    editor.putString("saved_heure_debut"+ indexPlan, heureDebut);
                                     editor.commit();}})
                             .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                                 public void onClick(DialogInterface dialog, int whichButton) {/* Do Nothing */ ;}})
                             .show(); }}});
         Button boutonHeureFin=(Button)findViewById(R.id.buttonHeureFin);
-        boutonHeureFin.setOnClickListener(new View.OnClickListener(){ // Notre classe anonyme
-            public void onClick(View view){ // et sa méthode !
+        boutonHeureFin.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
                 if (view.getId()==R.id.buttonHeureFin){
                     final EditText inputDialog = new EditText(MainActivity.this);
                     new AlertDialog.Builder(MainActivity.this)
@@ -184,7 +272,7 @@ public class MainActivity extends AppCompatActivity {
                                     heureFin = inputDialog.getText().toString();
                                     SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
                                     SharedPreferences.Editor editor = sharedPref.edit();
-                                    editor.putString("saved_heure_fin", heureFin);
+                                    editor.putString("saved_heure_fin"+ indexPlan, heureFin);
                                     editor.commit();}})
                             .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                                 public void onClick(DialogInterface dialog, int whichButton) {/* Do Nothing */ ;}})
@@ -197,10 +285,10 @@ public class MainActivity extends AppCompatActivity {
         int i=0; int j;
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode==12345 && resultCode==RESULT_OK) {
-            Uri selectedfile = data.getData(); //The uri with the location of the file
-            SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE); //getActivity().
+            Uri selectedfile = data.getData();
+            SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
             SharedPreferences.Editor editor = sharedPref.edit();
-            editor.putString("saved_uri_selected", selectedfile.toString());
+            editor.putString("saved_uri_selected"+ indexPlan, selectedfile.toString());
             editor.commit();
             chargePlanning(selectedfile);}}
 
@@ -246,7 +334,7 @@ public class MainActivity extends AppCompatActivity {
                 if (se.charAt(0)==charSousTitre) {
                     nbSousTitres++;}}
         } catch (FileNotFoundException e) {
-            mTextMessage.setText("echec lecture");
+            mTextMessage.setText(getText(R.string.msg_erreur_lecture)+" ("+ indexPlan +")");
             e.printStackTrace();}
       return;}
 
